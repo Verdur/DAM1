a1263b856a7223c5864caaeb93b364169d62ff9d
Verdur <56533908+Verdur@users.noreply.github.com>
2020-12-17 13:33:33 +0100

    clase 17/12
    Actualización positivos/negativos 15/12

--- prueba positivos negativos.c
+++ prueba positivos negativos.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+#define CERRAR 0
 main(){
 	int num1 = {0}, contpos = {0}, contneg = {0};
 	printf("Introduce un numeros, 0 para parar\n");
 	scanf ("%d", &num1);
 	fflush (stdin);
-	while (num1 !=0){
+	while (num1 !=CERRAR){
 		if (num1>0)
 			++contpos;
 		else
-			++contneg;
+			if (num1 != 0)
+				++contneg;
 	scanf ("%d", &num1);
 	fflush (stdin);	
 	}
