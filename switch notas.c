51734f1df2a65c5ecd129b876b96438d65757a94
Verdur <56533908+Verdur@users.noreply.github.com>
2020-12-17 14:12:25 +0100

    clase 17/12 bien
    switch notas mejorado

--- switch notas.c
+++ switch notas.c
@@ -8,26 +8,21 @@ main() {
 		fflush(stdin);
 	}while(nota < 0 || nota > 10);
 	switch (nota){
-		case 0 : printf("Insuficiente\n");
-			break;
-		case 1 : printf("Insuficiente\n");
-			break;
+		case 0 : 
+		case 1 : 
 		case 2 : printf("Insuficiente\n");
 			break;
-		case 3 : printf("Suspenso\n");
-			break;
+		case 3 :
 		case 4 : printf("Suspenso\n");
 			break;
 		case 5 : printf("Aprobado\n");
 			break;
 		case 6 : printf("Bien\n");
 			break;
-		case 7 : printf("Notable\n");
-			break;
+		case 7 : 
 		case 8 : printf("Notable\n");
 			break;
-		case 9 : printf("Sobresaliente\n");
-			break;
+		case 9 : 
 		case 10 : printf("Sobresaliente\n");
 			break;
 		default : printf("ERROR\n");
