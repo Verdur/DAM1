279b03f2b3c05151493772d66efe9d0accdfdaa0
Verdur <56533908+Verdur@users.noreply.github.com>
2020-12-22 18:58:53 +0100

    entrega fernando 22/12 FINAL
    Actividad entrega completa y comentada

--- entrega prog.c
+++ entrega prog.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
-
+#define STOP 0
+//SI SE CAMBIA EL NUMERO DE CONTROL PARA PARAR ESTE NO SERA CONTADO, SIEMPRE SE EXCLUYE EL NUMERO ESTABLECIDO PARA PARAR.
 main(){
+	//Declaracion Variables
 	int num1={0},sumnum = {0}, tam = {0}, sumtam = {0},i={0},f={0},totsum={0};
-	printf("Introduce el numero de tablas (0) para terminar\n");
+	//Pedimos un numero que siempre debera ser mayor que 0 no puede haber tablas negativas
+	printf("Introduce el numero de tablas (%d) para terminar\n", STOP);
+	do{
 	scanf("%d",&num1);
-	fflush (stdin);
-	while(num1!=0){
-		sumnum= num1 + sumnum;
-		scanf("%d",&num1);
-		fflush (stdin);
-	}
-	printf("Introduce el tamaño de la tabla (0) para terminar\n");
-	scanf("%d",&tam);
-	fflush (stdin);
-	while(tam!=0){
-		sumtam= tam + sumtam;
+	fflush (stdin);}while(num1 < 0);
+	//bucle mientras ya que nos puede introducir el stop desde el principio
+	while(num1 != STOP){
+		//pedimos el tamaño que tendrá cada tabla, solo ocurre cuando ya sabemos que vamos a imprimir x numero
+		printf("Introduce el tamaño de la tabla\n");
+		//de igual manera controlamos que sean positivos
+		do{
 		scanf("%d",&tam);
-		fflush (stdin);
+		fflush (stdin);}while(tam < 0);
+		totsum=0;
+		//dos bucles para anidados de forma que se hagan las tablas de multiplicar
+		for (i=1;i<=num1;++i){
+			for(f=1;f<=tam;++f){
+				totsum = totsum + (i*f);
+				printf ("%d * %d = %d\n",i,f,i*f);
+		}
+			printf ("====================\n");
+		}
+		printf ("La suma total es %d\n",totsum);
+		//paramos antes de limpiar pantalla para preguntar si queremos hacer otras tablas.
+		system ("pause");
+		system("cls");
+		printf("Introduce el numero de tablas (%d) para terminar\n",STOP);
+		do{
+			scanf("%d",&num1);
+			fflush (stdin);}while(num1 < 0);
 	}
-	for (i=1;i<=sumnum;++i){
-		for(f=1;f<=sumtam;++f){
-			totsum = totsum + (i*f);
-			printf ("%d * %d = %d\n",i,f,i*f);
-	}
-		printf ("--------------------------\n");
-	}
-	printf ("La suma total es %d",totsum);
+	//paramos antes de acabar ya que en caso de abrir el exe no tiene pause por defecto.
+	system ("pause");
+	
 	return 0;
 }
