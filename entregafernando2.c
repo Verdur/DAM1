ffc0e99b93be9e6d60bd2d6a7651177f3830c98a
Verdur <56533908+Verdur@users.noreply.github.com>
2021-01-30 14:02:07 +0100

    Add files via upload

--- entregafernando2.c
+++ entregafernando2.c
@@ -3,24 +3,29 @@
 #define TAM 50
 #define TAM1 50
 
-void subcadenas (char cadena[], char cadena2[]);
+void subcadenas (char cadena[], char cadena2[], int* oc);
 main(){
 	char cadena [TAM] = "", cadena2 [TAM1] = "";
+	int cont = {0};
 	gets (cadena);
 	fflush (stdin);
 	gets (cadena2);
 	fflush (stdin);
-	subcadenas (cadena,cadena2);
-	
+	subcadenas (cadena,cadena2,&cont);
+	printf ("La subcadena aparece %d veces en la cadena principal\n", cont);
 	system ("pause");
 	return 0;
 }
 
-void subcadenas (char cadena[], char cadena2[]){
-	
-	 if (strstr(cadena,cadena2))
-	 	printf("Ocurrencia encontrada");
-	
-	
+void subcadenas (char cadena[], char cadena2[], int* oc){
+	do{
+		if (strstr(cadena,cadena2)){
+			cadena = strstr(cadena,cadena2);
+			(*oc)++;
+			cadena[0]=' ';
+			/*printf ("%s",cadena);
+			system ("pause");*/
+		}	
+	}while(strstr(cadena,cadena2));
 	return;
 }
